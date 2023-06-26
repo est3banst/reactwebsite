99a58e891dee56794a1173188513434a472ee9c0
est3banst <estebeeen@outlook.com>
2023-06-25 23:39:54 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import Gallery from './gallery';
 import Hero from './hero';
+import Section from './section';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
     <Gallery />
+    <Section/>
     <Hero />
   </React.StrictMode>
 );
